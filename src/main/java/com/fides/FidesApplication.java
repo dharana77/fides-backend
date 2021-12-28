7d0d3e87f74620fbb7b8bccafad5b4a6a46eeb38
dharana <dharana7723@gmail.com>
2021-12-28 23:35:31 +0900

    chore: init project via 스프링부트

---
+++ src/main/java/com/fides/FidesApplication.java
@@ -0,0 +1,13 @@
+package com.fides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FidesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fides/FidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
