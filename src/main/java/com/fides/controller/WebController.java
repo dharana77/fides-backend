4b2692b15bf4c3a6d4048e34e84388b9b710c107
dharana <dharana7723@gmail.com>
2021-12-29 18:39:35 +0900

    feat: vue Controller 추가

---
+++ src/main/java/com/fides/controller/WebController.java
@@ -0,0 +1,14 @@
+package com.fides.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class WebController {
+    @GetMapping("/vue")
+    public String vue(){
+        return "vue/index";
+    }
+}
