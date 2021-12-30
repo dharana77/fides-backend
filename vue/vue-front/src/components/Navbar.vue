e5913c20e31dbe756c81b848854e826aadad40a7
dharana <dharana7723@gmail.com>
2021-12-30 22:46:54 +0900

    fix: style 주석 제거 및 ident 오류 수정

--- vue/vue-front/src/components/Navbar.vue
+++ vue/vue-front/src/components/Navbar.vue
@@ -1,21 +1,21 @@
 <template>
-  <nav id="vue">
-    <div class="menu-item"><router-link to="/">Home</router-link></div>
-    <div class="menu-item"><router-link to="/using">Using fides</router-link></div>
-    <div class="menu-item"><router-link to="/community">Community</router-link></div>
-    <Services title="Services"/>
-  </nav>
+    <nav id="vue">
+        <div class="menu-item"><router-link to="/">Home</router-link></div>
+        <div class="menu-item"><router-link to="/using">Using fides</router-link></div>
+        <div class="menu-item"><router-link to="/community">Community</router-link></div>
+        <Services title="Services"/>
+    </nav>
 </template>
 
 <script>
-  import Services from '../components/Services'
-  export default {
-    name: 'navbar',
-    components:{
-      Services
-    },
+import Services from '../components/Services'
+export default {
+  name: 'navbar',
+  components: {
+    Services
   }
+}
 </script>
+
 <style>
-  #for styling
 </style>
