9f195cee3b2933cb1539d41fc369972eb5a5da2c
dharana <dharana7723@gmail.com>
2022-01-01 01:26:43 +0900

    feat: 체스 이미지 components2에 추가

--- vue/vue-front/src/App.vue
+++ vue/vue-front/src/App.vue
@@ -3,17 +3,20 @@
     <Navbar/>
     <router-view/>
     <Component1/>
+    <Component2/>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
 import Component1 from '@/components/Component1'
+import Component2 from '@/components/Component2'
 export default {
   name: 'App',
   components: {
     Navbar,
-    Component1
+    Component1,
+    Component2
   }
 }
 </script>
---
+++ vue/vue-front/src/components/Component2.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="component2">
+      <img id="chess1" src="../assets/Chess1.png">
+      <img id="chess2" src="../assets/Chess2.png">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'component2',
+  components: {
+  }
+}
+</script>
+
+<style>
+#chess1 {
+  height: 267.17490145529985px;
+  width: 113.27063209377528px;
+  top: 741.300537109375px;
+  border-radius: 0px;
+
+  position: absolute;
+  left: 89.8%;
+  right: 3.12%;
+  bottom: 78.58%;
+  background-color:white;
+}
+#chess2 {
+  position: absolute;
+  height: 247.28165346559211px;
+  width: 119.18116349536974px;
+  left: 150px;
+  top: 1485.837646484375px;
+  border-radius: 0px;
+  background: white;
+}
+</style>
