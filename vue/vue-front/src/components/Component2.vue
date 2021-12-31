fea405daf62222e82da0950fb57680d5bc34e729
dharana <dharana7723@gmail.com>
2022-01-01 02:11:10 +0900

    feat: component2 그림 추가

--- vue/vue-front/src/components/Component2.vue
+++ vue/vue-front/src/components/Component2.vue
@@ -2,6 +2,26 @@
     <div id="component2">
       <img id="chess1" src="../assets/Chess1.png">
       <img id="chess2" src="../assets/Chess2.png">
+      <h1>Lean More About Fides</h1>
+      <p>Fides is the new wave for Investing crypto asset, We can devide the right
+         and the seed money</p>
+      <img id ="info" src="../assets/arrow_button.png">
+      <img id = "info_icon" src="../assets/info_icon.png">
+      <h1>Become Speaker Tier</h1>
+      <p>Fides is the new wave for Investing crypto asset, We can devide the right
+         and the seed money</p>
+      <img id ="tier" src="../assets/arrow_button.png">
+      <img id = "tier_icon" src="../assets/tier_icon.png">
+      <h1>Build Your Community</h1>
+      <p>Fides is the new wave for Investing crypto asset, We can devide the right
+         and the seed money</p>
+      <img id ="subdao" src="../assets/arrow_button.png">
+      <img id = "subdao_icon" src="../assets/subdao_icon.png">
+      <h1>Join Out Community</h1>
+      <p>Fides is the new wave for Investing crypto asset, We can devide the right
+         and the seed money</p>
+      <img id ="community" src="../assets/arrow_button.png">
+      <img id = "community_icon" src="../assets/community_icon.png">
     </div>
 </template>
 
@@ -14,6 +34,13 @@ export default {
 </script>
 
 <style>
+#component2{
+  width: 1600px;
+  height: 1048px;
+  left: 0px;
+  top: 607px;
+  background: #EDEDF2;
+}
 #chess1 {
   height: 267.17490145529985px;
   width: 113.27063209377528px;
@@ -24,7 +51,7 @@ export default {
   left: 89.8%;
   right: 3.12%;
   bottom: 78.58%;
-  background-color:white;
+  background: #EDEDF2;
 }
 #chess2 {
   position: absolute;
@@ -33,6 +60,45 @@ export default {
   left: 150px;
   top: 1485.837646484375px;
   border-radius: 0px;
+  background: #EDEDF2;
+}
+
+#info, #tier, #subdao,#community{
   background: white;
+}
+#info_icon, #tier_icon, #subdao_icon, #community_icon{
+  background:white;
+}
+#info_icon{
+  width: 308.18px;
+  height: 205.1px;
+  left: 460.36px;
+  top: 947px;
+  transform: rotate(10.78deg);
+}
+#tier_icon{
+  width: 235.78px;
+  height: 183.26px;
+  left: 922px;
+  top: 855.56px;
+  transform: rotate(deg);
+}
+#subdao_icon{
+  left: 35.25%;
+  right: 48.35%;
+  top: 30.56%;
+  bottom: 65.52%;
+  height: 184.4870147705078px;
+  width: 262.3983459472656px;
+  left: 564px;
+  top: 1439px;
+  border-radius: 0px;
+
+}
+#community_icon{
+  width: 303.36px;
+  height: 190.28px;
+  left: 860px;
+  top: 1373px;
 }
 </style>
