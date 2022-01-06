c88c20f90187ae4a80ad1b8c3481f42a5697cead
dharana <dharana7723@gmail.com>
2022-01-07 05:16:56 +0900

    fix:comopnent7 수정중

--- vue/vue-front/src/App.vue
+++ vue/vue-front/src/App.vue
@@ -20,6 +20,7 @@ import Component3 from '@/components/Component3'
 import Component4 from '@/components/Component4'
 import Component5 from '@/components/Component5'
 import Component6 from '@/components/Component6'
+import Component7 from '@/components/Component7'
 export default {
   name: 'App',
   components: {
@@ -29,16 +30,15 @@ export default {
     Component3,
     Component4,
     Component5,
-    Component6
+    Component6,
+    Component7
   }
 }
 </script>
 
 <style>
 #app {
-  width: 100%;
   text-align: center;
   overflow-x: hidden;
-
 }
 </style>
--- vue/vue-front/src/components/Component7.vue
+++ vue/vue-front/src/components/Component7.vue
@@ -1,49 +1,39 @@
 <template>
     <div id="component7">
+        <img id="component7-whitepaper-img" src="../assets/component7/whitepaper.png">
+        <p id="component7-titles">More Information<br>
+         <span id="component7-contents">Our Fides is more advanced and<br>
+         democratic investment guild for<br> capital increase</span></p>
 
-      <div id= "text-content-wrapper">
-        <img id="whitepaper-img" src="../assets/info/whitepaper.png">
-        <div id="main-text">
-          <span id="content-title" class="infoText">More Information</span><br>
-          <span id="content-text" class="infoText">Our Fides is more advanced and<br>
-           democratic investment guild for<br> capital increase</span>
-
-          <div id="explains">
-            <div id="explain1">
-              <br>
-              <img class="icons" id="icon1" src="../assets/info/icon1.png">
-              <h5>1/n System</h5>
-              <h5>p.2</h5>
-            </div>
-            <div id="explain2">
-              <img class="icons" id="icon2" src="../assets/info/icon2.png">
-              <h5>Tiered dao community</h5>
-              <h5>p.3</h5>
-            </div>
-            <div id="explain3">
-              <img class="icons" id="icon3" src="../assets/info/icon3.png">
-              <h5>Subdao system</h5>
-              <h5>p.4</h5>
-            </div>
-            <div id="explain4">
-              <img class="icons" id="icon2" src="../assets/info/icon4.png">
-              <h5>Lend system</h5>
-              <h5>p.5</h5>
-            </div>
-            <div id="explain5">
-              <img class="icons" id="icon2" src="../assets/info/icon5.png">
-              <h5>Dao driven investment system</h5>
-              <h5>p.6</h5>
-            </div>
-            <div id="explain6">
-              <img class="icons" id="icon2" src="../assets/info/icon6.png">
-              <h5>Bounty system</h5>
-              <h5>p.7</h5>
-            </div>
-          </div>
+        <img id="component7-icon1" src="../assets/component7/icon1.png">
+        <h5>1/n System</h5>
+        <h5>p.2</h5>
+        <div id="explain2">
+          <img class="icons" id="icon2" src="../assets/component7/icon2.png">
+          <h5>Tiered dao community</h5>
+          <h5>p.3</h5>
+        </div>
+        <div id="explain3">
+          <img class="icons" id="icon3" src="../assets/component7/icon3.png">
+          <h5>Subdao system</h5>
+          <h5>p.4</h5>
+        </div>
+        <div id="explain4">
+          <img class="icons" id="icon2" src="../assets/component7/icon4.png">
+          <h5>Lend system</h5>
+          <h5>p.5</h5>
+        </div>
+        <div id="explain5">
+          <img class="icons" id="icon2" src="../assets/component7/icon5.png">
+          <h5>Dao driven investment system</h5>
+          <h5>p.6</h5>
+        </div>
+        <div id="explain6">
+          <img class="icons" id="icon2" src="../assets/component7/icon6.png">
+          <h5>Bounty system</h5>
+          <h5>p.7</h5>
         </div>
       </div>
-    </div>
 </template>
 
 <script>
@@ -56,64 +46,53 @@ export default {
 
 <style>
 #component7{
-  position: relative;
-  width: 100%;
+  position: absolute;
   height: 530px;
-  text-align: left;
+  left: 0px;
+  top: 3867px;
+  background: #FFFFFF;
 }
-
-#whitepaper-img{
-  position: relative;
+#component7-whitepaper-img{
+  position: absolute;
   width: 250.93px;
   height: 308.09px;
-  top: 100px;
-  left: 20.05%;
-  box-sizing: border-box;
-  background: rgba( 255, 255, 255, 0 );
+  left: 310px;
+  top: 163.83px;
 }
 
-#text-content-wrapper{
-}
-
-#main-text{
-  position: relative;
-   text-align: left;
-   top: -200px;
-   right: -700px;
-}
-.icons{
-  background: white;
-  height: 18px;
-  width: 18px;
-  border-radius: 0px;
-}
-#explains{
-  text-align: left;
-}
-#explain1 img, #explain2 img, #explain3 img, #explain4 img, #explain5 img, #explain6 img{
-  vertical-align: middle;
-}
-#explain1 h5, #explain2 h5, #explain3 h5, #explain4 h5, #explain5 h5, #explain6 h5{
-  display: inline;
-  vertical-align: middle;
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 20px;
-  line-height: 24px;
-}
-
-.infoText{
+#component7-titles{
+  position: absolute;
+  width: 480px;
+  height: 140px;
+  left: 810px;
+  top: 81px;
   font-family: Roboto;
   font-style: normal;
   font-weight: 900;
   font-size: 30px;
   line-height: 36px;
   /* or 120% */
-}
-
-#content-title{
-  white-space: nowrap;
   color: #3B7E74;
+  text-align:left;
+}
+#component7-contents{
+  font-family: Roboto;
+  font-style: normal;
+  font-size: 30px;
+  font-weight: 500;
+  line-height: 36px;
+  /* or 120% */
+  color: black;
+  text-align:left;
+}
+#component7-icon1{
+  position: absolute;
+  height: 18px;
+  width: 18px;
+  left: 810px;
+  top: 87.51%;
+  bottom: 12.1%;
+  border-radius: 0px;
+
 }
 </style>
