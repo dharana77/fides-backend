6be034de8360f7b9424fe1c081c0467eda188cd9
dharana <dharana7723@gmail.com>
2022-01-09 00:11:18 +0900

    fix: component3 css 수정

--- vue/fides/src/components/Component3.vue
+++ vue/fides/src/components/Component3.vue
@@ -1,37 +1,43 @@
 <template>
     <div id="component3">
-          <div id="fides-explain-first">
+          <div id="component3-explain-box">
             <span class="fides-explain-title">Why Fides?</span><br>
             <span class="fides-explain-text">Fides is the new wave for Investing crypto asset, We can
             devide the right and the seed money.<br>
             And then we share our profits with less capital</span>
           </div>
-          <div id="explain-contents1">
-            <h1 class="contents1-title">1/N Investment</h1>
-            <p class="contents1-text">Fides is the new wave for Investing crypto
+
+          <div id="component3-explain-contents1">
+            <h1 class="contents1-title" id="component3-box1-title">1/N Investment</h1>
+            <p class="contents1-text" id="component3-box1-text">Fides is the new wave for Investing crypto
             asset, Our strong point is our App can devide
             the right and the seed money of the investment ,
             At last we can share risk and advantage of
             investment.</p>
-            <h3 class="detail">View Detail</h3>
+            <h3 class="detail" id="component3-box1-detail">View Detail</h3>
+            <img id="component3-line1">
           </div>
-          <div id="explain-contents2">
-            <h1 class="contents2-title">Tiered Dao System</h1>
-            <p class="contents2-text">Fides is the new wave for Investing crypto
+
+          <div id="component3-explain-contents2">
+            <h1 class="contents2-title" id="component3-box2-title">Tiered Dao System</h1>
+            <p class="contents2-text" id="component3-box2-text">Fides is the new wave for Investing crypto
             asset, Our strong point is our App can devide
             the right and the seed money of the investment ,
             At last we can share risk and advantage of
             investment.</p>
-            <h3 class="detail">View Detail</h3>
+            <h3 class="detail" id="component3-box2-detail">View Detail</h3>
+            <img id="component3-line2">
           </div>
-          <div id="explain-contents3">
-            <h1 class="contents3-title">Lend System</h1>
-            <p class="contents3-text">Fides is the new wave for Investing crypto
+
+          <div id="component3-explain-contents3">
+            <h1 class="contents3-title" id="component3-box3-title">Lend System</h1>
+            <p class="contents3-text" id="component3-box3-text">Fides is the new wave for Investing crypto
             asset, Our strong point is our App can devide
             the right and the seed money of the investment ,
             At last we can share risk and advantage of
             investment.</p>
-            <h3 class="detail">View Detail</h3>
+            <h3 class="detail" id="component3-box3-detail">View Detail</h3>
+            <img id="component3-line3">
           </div>
     </div>
 </template>
@@ -48,7 +54,7 @@ export default {
 #component3{
 }
 
-#fides-explain-first{
+#component3-explain-box{
   position: absolute;
   width: 780px;
   height: 142px;
@@ -63,6 +69,7 @@ export default {
   color: #0F8D7C;
   text-align:left;
 }
+
 .fides-explain-title{
   font-family: Roboto;
   font-style: normal;
@@ -81,7 +88,7 @@ export default {
   color: black;
 }
 
-#explain-contents1{
+#component3-explain-contents1{
  position: absolute;
  width: 380px;
  height: 290px;
@@ -90,7 +97,7 @@ export default {
  background: #F9F9F9;
  border-radius: 20px;
  }
- #explain-contents2{
+#component3-explain-contents2{
   position: absolute;
   width: 380px;
   height: 290px;
@@ -100,7 +107,7 @@ export default {
   background: #F9F9F9;
   border-radius: 20px;
 }
-#explain-contents3{
+#component3-explain-contents3{
   position: absolute;
   width: 380px;
   height: 290px;
@@ -138,5 +145,57 @@ export default {
   line-height: 14px;
   /* or 87% */
   color: #96BEB9;
+}
+#component3-line1{
+  position: absolute;
+  width: 340px;
+  height: 1px;
+  left: 20px;
+  top: 229px;
+  background: #96BEB9;
+}
+#component3-line2{
+ position: absolute;
+ width: 340px;
+ height: 1px;
+ left: 20px;
+ top: 229px;
+ background: #96BEB9;
+}
+#component3-line3{
+  position: absolute;
+  width: 340px;
+  height: 1px;
+  left: 20px;
+  top: 229px;
+  background: #96BEB9;
+}
+#component3-box1-detail{
+  position: absolute;
+  left: 20px;
+  top: 242px;
+}
+#component3-box1-detail, #component3-box2-detail, #component3-box3-detail{
+  position: absolute;
+  left: 20px;
+  top: 242px;
+}
+#component3-box1-title, #component3-box2-title, #component3-box3-title{
+  position: absolute;
+  text-align: left;
+  left: 20px;
+}
+#component3-box1-text, #component3-box2-text, #component3-box3-text{
+  position: absolute;
+  text-align: left;
+  left: 20px;
+  top: 109px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  /* or 125% */
+  color: #606060;
 }
 </style>
