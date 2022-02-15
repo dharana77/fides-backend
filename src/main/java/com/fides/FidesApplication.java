f959c58811600b6286fa2d5fdf5f7bad97fd304e
dharana <dharana7723@gmail.com>
2022-02-16 00:09:37 +0900

    feat: mobile 분기 추가

--- src/main/java/com/fides/FidesApplication.java
+++ src/main/java/com/fides/FidesApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FidesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FidesApplication.class, args);
 	}
-
 }
--- src/main/java/com/fides/controller/WebController.java
+++ src/main/java/com/fides/controller/WebController.java
@@ -3,12 +3,22 @@ package com.fides.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @AllArgsConstructor
 public class WebController {
+    private static final String IS_MOBILE = "MOBI";
+
     @GetMapping("/")
-    public String vue(){
+    public String detectDevice(HttpServletRequest request) {
+        String userAgent = request.getHeader("User-Agent").toUpperCase();
+        if(userAgent.indexOf(IS_MOBILE) > -1) {
+            return "mobile/index";
+        }
         return "vue/index";
     }
 }
