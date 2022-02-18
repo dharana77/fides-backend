51615da6b10adef03661f667f78dc8e93ec5c0f8
dharana <dharana7723@gmail.com>
2022-02-19 03:18:10 +0900

    feat: component 4 모바일 추가

--- mobile/mobile-front/src/App.vue
+++ mobile/mobile-front/src/App.vue
@@ -3,6 +3,7 @@
     <Navbar/>
     <Component1/>
     <Component2/>
+    <Component3/>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import Navbar from '@/components/Navbar'
 import Component1 from '@/components/Component1'
 import Component2 from '@/components/Component2'
+import Component3 from '@/components/Component3'
 export default {
   name: 'App',
   components: {
       Navbar,
       Component1,
       Component2,
+      Component3,
     }
 }
 </script>
---
+++ mobile/mobile-front/src/components/Component3.vue
@@ -0,0 +1,274 @@
+<template>
+    <div id="component3">
+      <div id="component3-title">
+        The missions of Fides
+      </div>
+      <div id="component3-content">
+        Fides is Decentralization of Venture Capital, and distribution of the gain back to the project participant
+        The more specific development plan is followed.
+      </div>
+      <div id="component3-box1">
+        <div id="component3-box1-title">Beta Profit</div>
+        <div id="component3-box1-content">Beta profit is a part of the index fund system. Fides aims to follow at least the market average gain, and we will contrive the ways of growth to meet the mission statement.
+                                          Through this, we can prevent the risk of the bear market, and expect continuous growth.
+        </div>
+        <img id="component3-box1-line" src="../assets/component3-line.png"></img>
+        <div id="component3-box1-detail">View Detail</div>
+      </div>
+
+      <div id="component3-box2">
+        <div id="component3-box2-title">1/N Investment</div>
+        <div id="component3-box2-content">You can purchase the part of the asset, not the whole.
+                                                                                    Through this, you will have an autonomous marketplace, and the owners will have opportunities to buy and sell their ownership.
+        </div>
+        <img id="component3-box2-line" src="../assets/component3-line.png"></img>
+        <div id="component3-box2-detail">View Detail</div>
+      </div>
+
+      <div id="component3-box3">
+        <div id="component3-box3-title">Project launch & IDO</div>
+        <div id="component3-box3-content">Initiation of users' projects and participation in other projects are possible.
+                                          Depending on the conditions of the investment, you will have the right to participate in the project as a beta tester.
+        </div>
+        <img id="component3-box3-line" src="../assets/component3-line.png"></img>
+        <div id="component3-box3-detail">View Detail</div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'component3',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+#component3{
+  position: absolute;
+  width: 2560px;
+}
+#component3-title{
+  position: absolute;
+  width: 335px;
+  height: 28px;
+  left: 22px;
+  top: 2684px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 30px;
+  line-height: 36px;
+  /* or 120% */
+  color: #0F8D7C;
+}
+#component3-content{
+  position: absolute;
+  width: 335px;
+  height: 173px;
+  left: 22px;
+  top: 2734px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 30px;
+  /* or 136% */
+  color: #606060;
+}
+#component3-box1{
+  position: absolute;
+  width: 335px;
+  height: 312px;
+  left: 22px;
+  top: 2937px;
+
+  background: #F9F9F9;
+  box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.15);
+  border-radius: 20px;
+}
+#component3-box1-title{
+  position: absolute;
+  width: 295px;
+  height: 23px;
+  left: 20px;
+  top: 30px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 24px;
+  line-height: 26px;
+  /* or 108% */
+  color: #1CAE9A;
+}
+#component3-box1-content{
+  position: absolute;
+  width: 295px;
+  height: 155px;
+  left: 20px;
+  top: 71px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  /* or 125% */
+  color: #606060;
+}
+#component3-box1-line{
+  position:absolute;
+  width: 295px;
+  height: 1px;
+  left: 20px;
+  top: 253px;
+  background: #96BEB9;
+}
+#component3-box1-detail{
+  position: absolute;
+  width: 295px;
+  height: 12px;
+  left: 20px;
+  top: 270px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 14px;
+  /* or 87% */
+  color: #96BEB9;
+}
+
+#component3-box2{
+  position: absolute;
+  width: 335px;
+  height: 272px;
+  left: 22px;
+  top: 3279px;
+
+  background: #F9F9F9;
+  box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.15);
+  border-radius: 20px;
+}
+#component3-box2-title{
+  position: absolute;
+  width: 295px;
+  height: 23px;
+  left: 20px;
+  top: 30px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 24px;
+  line-height: 26px;
+  /* or 108% */
+
+  color: #1CAE9A;
+}
+#component3-box2-content{
+  position: absolute;
+  width: 295px;
+  height: 115px;
+  left: 21px;
+  top: 71px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  /* or 125% */
+
+  color: #606060;
+}
+#component3-box2-line{
+  position:absolute;
+  width: 295px;
+  height: 1px;
+  left: 20px;
+  top: 213px;
+  background: #96BEB9;
+}
+#component3-box2-detail{
+  position: absolute;
+  width: 295px;
+  height: 12px;
+  left: 20px;
+  top: 230px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 14px;
+  /* or 87% */
+  color: #96BEB9;
+}
+
+#component3-box3{
+  position: absolute;
+  width: 335px;
+  height: 272px;
+  left: 22px;
+  top: 3581px;
+
+  background: #F9F9F9;
+  box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.15);
+  border-radius: 20px;
+}
+#component3-box3-title{
+  position: absolute;
+  width: 295px;
+  height: 23px;
+  left: 20px;
+  top: 30px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 24px;
+  line-height: 26px;
+  /* or 108% */
+
+  color: #1CAE9A;
+}
+#component3-box3-content{
+  position: absolute;
+  width: 295px;
+  height: 115px;
+  left: 21px;
+  top: 71px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  /* or 125% */
+
+  color: #606060;
+}
+#component3-box3-line{
+  position:absolute;
+  width: 295px;
+  height: 1px;
+  left: 20px;
+  top: 213px;
+  background: #96BEB9;
+}
+#component3-box3-detail{
+  position: absolute;
+  width: 295px;
+  height: 12px;
+  left: 20px;
+  top: 230px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 14px;
+  /* or 87% */
+  color: #96BEB9;
+}
+</style>
