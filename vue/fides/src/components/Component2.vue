3336ed7696b38e74a9223a66e5295a9ed079f749
dharana <dharana7723@gmail.com>
2022-02-23 04:19:06 +0900

    fix: animation component2 효과 변경

--- vue/fides/src/components/Component2.vue
+++ vue/fides/src/components/Component2.vue
@@ -1,27 +1,27 @@
 <template>
     <div id="component2">
 
-      <div id="component2-box1" class="animate__animated animate__flipInX">
+      <div id="component2-box1" class="animate__animated animate__rotateIn">
         <h1 id="component2-box1-title">What is Fides?</h1>
         <p id="component2-box1-text">Fides is innovative venture capital for the cryptocurrency managed by the investor community's collective intelligence which lowers the risk and maximizes the gain.</p>
         <img id = "component2-box1-icon" src="../assets/component2/box1-icon.png">
       </div>
 
-      <div id="component2-box2" class="animate__animated animate__flipInX">
+      <div id="component2-box2" class="animate__animated animate__rotateInDownRight">
         <h1 id="component2-box2-title">Be the community member</h1>
         <p id="component2-box2-text">You will have an opportunity to be a part of this capital as an investor, tester, and founding member.
 You not only can do direct investment but also you can do indirect investment.</p>
         <img id = "component2-box2-icon" src="../assets/component2/box2-icon.png">
       </div>
 
-      <div id="component2-box3" class="animate__animated animate__flipInY">
+      <div id="component2-box3" class="animate__animated animate__rotateIn">
         <h1 id="component2-box3-title">Shine your distinctive characters</h1>
         <p id="component2-box3-text">Fides assists you to maximize your investment capability through the "Tier" system.
 Also, this will allow us to do more effective communication in DAO.</p>
         <img id = "component2-box3-icon" src="../assets/component2/box3-icon.png">
       </div>
 
-      <div id="component2-box4" class="animate__animated animate__flipInY">
+      <div id="component2-box4" class="animate__animated animate__zoomIn">
         <h1 id="component2-box4-title">Research and Discussion about the investment.</h1>
         <p id="component2-box4-text">Vitalization of DAO is possible through active discussion, not just for the voting.
 This allows the guilds to be more productive with assurance, and stimulate the faster growth.</p>
@@ -74,7 +74,7 @@ export default {
   top: 70px;
   background: #F5F8FC;
   border-radius: 20px;
-  animation-duration:2.5s;
+  animation-duration:3.5s;
 }
 #component2-box1:hover{
  box-shadow: rgba(0, 0, 0, 0.12) 0px 0px 18px;
@@ -87,7 +87,7 @@ export default {
   top: 70px;
   border-radius: 20px;
   background: #F5F8FC;
-  animation-duration:2.5s;
+  animation-duration:2.3s;
 }
 #component2-box2:hover{
  box-shadow: rgba(0, 0, 0, 0.12) 0px 0px 18px;
@@ -100,7 +100,7 @@ export default {
   top: 610px;
   background: #F5F8FC;
   border-radius: 20px;
-  animation-duration:2.5s;
+  animation-duration:3.5s;
 }
 #component2-box3:hover{
  box-shadow: rgba(0, 0, 0, 0.12) 0px 0px 18px;
@@ -113,7 +113,7 @@ export default {
   top: 490px;
   background: #F5F8FC;
   border-radius: 20px;
-  animation-duration:2.5s;
+  animation-duration:3.5s;
 }
 #component2-box4:hover{
  box-shadow: rgba(0, 0, 0, 0.12) 0px 0px 18px;
