6a4ef34c95288b32e847a24aa58c645874dfb577
dharana <dharana7723@gmail.com>
2022-03-18 22:59:07 +0900

    fix: 리다이렉트 setport 80으로 수정

--- src/main/java/com/fides/HttpConfig.java
+++ src/main/java/com/fides/HttpConfig.java
@@ -36,7 +36,7 @@ public class HttpConfig {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
         connector.setSecure(false);
-        connector.setPort(8080);
+        connector.setPort(80);
         connector.setRedirectPort(443);
         return connector;
     }
